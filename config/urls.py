eb8acf27d70067ddfb4be3c22b7d275259a84880
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-03-11 22:41:20 +0300

    - Обновлены базовые urls проекта;

--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+                  # base urls
+                  path("admin/", admin.site.urls),
+
+                  # app urls
+                  path("users/", include("users.urls", namespace="users")),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
