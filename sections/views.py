1a9623b3636d8b2ea7d04fe6cf5267cc8c8a4141
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-03-12 20:36:32 +0300

    - Написан CRUD для модели Content; - Прописаны urls для Content; - Изменили имя сериализатора.

--- sections/views.py
+++ sections/views.py
@@ -5,8 +5,7 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from sections.models import Section, Content
 from sections.permissions import IsModerator, IsSuperUser
 from sections.serializers.section_serializers import SectionSerializer, SectionListSerializer
-from sections.serializers.content_serializers import (ContentSerializer, ContentSectionSerializer,
-                                                      ContentSectionListSerializer)
+from sections.serializers.content_serializers import (ContentSerializer, ContentListSerializer)
 from sections.paginators import SectionPaginator, ContentPaginator
 
 
@@ -39,3 +38,32 @@ class SectionDestroyAPIView(DestroyAPIView):
     queryset = Section.objects.all()
     # permission_classes = (IsAuthenticated, IsSuperUser)
 
+
+class ContentListAPIView(ListAPIView):
+    serializer_class = ContentListSerializer
+    queryset = Content.objects.all()
+    # permission_classes = (IsAuthenticated,)
+    pagination_class = ContentPaginator
+
+
+class ContentCreateAPIView(CreateAPIView):
+    serializer_class = ContentSerializer
+    # permission_classes = (IsAuthenticated, IsModerator | IsSuperUser)
+
+
+class ContentRetrieveAPIView(RetrieveAPIView):
+    serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+    # permission_classes = (IsAuthenticated,)
+
+
+class ContentUpdateAPIView(UpdateAPIView):
+    serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+    # permission_classes = (IsAuthenticated, IsModerator | IsSuperUser)
+
+
+class ContentDestroyAPIView(DestroyAPIView):
+    serializer_class = ContentSerializer
+    queryset = Content.objects.all()
+    # permission_classes = (IsAuthenticated, IsSuperUser)
