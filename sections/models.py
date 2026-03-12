49f1cec6921c8e07231a1fd14abd2aaded7af23d
Dmitriy Kulikov <dimonkulikov@mail.ru>
2026-03-12 19:22:39 +0300

    - Добавлена модель Section ; - Добавлена модель Content; - Написаны сериализаторы для обеих моделей.

--- sections/models.py
+++ sections/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from users.models import NULLABLE
+
+
+class Section(models.Model):
+    title = models.CharField(verbose_name=_('Title'), max_length=150, unique=True)
+    description = models.TextField(verbose_name=_('Description'), **NULLABLE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Section')
+        verbose_name_plural = _('Sections')
+        ordering = ['id']
+
+
+class Content(models.Model):
+    section = models.ForeignKey(Section, verbose_name=_('Section'), on_delete=models.CASCADE)
+    title = models.CharField(verbose_name=_('Title'), max_length=150, unique=True)
+    content = models.TextField(verbose_name=_('Content'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = _('Content')
+        verbose_name_plural = _('Content')
+        ordering = ['id']
